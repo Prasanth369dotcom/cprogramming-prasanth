c8eeceb1fa2ccfec22a8d2c1213c35111bcfa4ca
Prasanth369 <122433399+Prasanth369dotcom@users.noreply.github.com>
2023-02-01 00:38:11 -0800

    Create p4.c

---
+++ p4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num,rev_num=0,remainder;
+	printf("enter the number to reverse");
+	scanf("%d",&num);
+	while(num!=0)
+	{
+		remainder=num%10;
+		rev_num=rev_num*10+remainder;
+		num=num/10;
+	}
+	printf("the reversed number is:%d",rev_num);
+	getch();
+	}
