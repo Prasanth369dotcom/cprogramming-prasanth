640995bfe40adabccb204176bb882c2a26eff4c3
Prasanth369 <122433399+Prasanth369dotcom@users.noreply.github.com>
2023-02-01 00:38:51 -0800

    Create p5.c

---
+++ p5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int first,second,*p,*q,sum;
+	printf("enter two integers to add\n");
+	scanf("%d%d",&first,&second);
+	p=&first;
+	q=&second;
+	sum=*p+*q;
+	printf("sum of numbers=%d\n",sum);
+	return 0;
+}
