a733a07d1f380d5db09c62c01cf53f63ac1a9fd9
Prasanth369 <122433399+Prasanth369dotcom@users.noreply.github.com>
2023-02-01 00:41:36 -0800

    Create p8.c

---
+++ p8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,n;
+	char a;
+	printf("enter character to be printed:");
+	scanf("%c",&a);
+	printf("\n max no of times to be printed:");
+	scanf("&d",&n);
+	for(i=1;i<=n;i++)
+	{
+	}
+	for(j=1;j<=i;j++)
+	{
+		printf("%c",a);
+		}
+		printf("\n");
+		}
+		getch();
