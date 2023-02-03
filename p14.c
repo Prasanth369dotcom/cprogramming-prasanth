d01099bfc9739798b8b0376cb792ba82803804a2
Prasanth369 <122433399+Prasanth369dotcom@users.noreply.github.com>
2023-02-02 23:51:35 -0800

    Create p14.c
    factorial

---
+++ p14.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int x,fact=1,n;
+	printf("enter a number to find factorial");
+	scanf("%d",&n);
+	for(x=1;x<=n;x++)
+		fact=fact*x;
+		printf("the factorial of a number is %d",n,fact);
+		return 0;
+}
