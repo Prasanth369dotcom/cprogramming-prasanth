ed606bb167c32280824a2a89e06464affe935391
Prasanth369 <122433399+Prasanth369dotcom@users.noreply.github.com>
2023-02-02 23:58:13 -0800

    Create p15.c
    sum of digits

---
+++ p15.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,sum=0,m;
+	printf("enter any number");
+	scanf("%d",&n);
+	while(n>0)
+{
+	m=n%10;
+	sum=sum+m;
+	n=n/10;
+}
+printf("sum of digits=%d",sum);
+return 0;
+}
